cea60b2964fe114d694d12c943cc24076eeb4e97
unknown <Mboubiss@.(none)>
2013-04-17 14:45:36 -0400

    last change create content for the others menu add code source

---
+++ pages/dahira/projet.php
@@ -0,0 +1,53 @@
+<div class="descriptionProjetKeurSerigneTouba">
+
+	<h2>Justification du projet :</h2>
+	Le projet doit sa pertinence aux raisons ci-ci-dessous :
+	<ul>
+		<li>Agrandissement des membres de la la fondation « Nourou Darayni »</li>
+		<li>Besoin croissant en éducation religieuse</li>
+		<li>Coût du loyer de plus en plus cher.</li>
+	</ul>
+	<h2>Description du complexe :</h2>
+	Le complexe abritera les locaux suivants :
+	<ul>
+		<li>Une salle de prière pour hommes</li>
+		<li>Une salle de prière pour femmes</li>
+		<li>Des lieux d’ablution séparés pour</li> 
+	</ul>
+	hommes et femmes
+	<ul>
+		<li>Une salle de conférence</li>
+		<li>Une bibliothèque</li>
+		<li>Des salles de classe</li>
+		<li>Des chambres de passage.</li>
+	</ul>
+	<h2>Objectifs du projet :</h2>
+	<ul>
+		<li>Développer l’éducation religieuse et la pratique des préceptes de l’islam.</li>
+		<li>Perpétuer  les enseignements du Prophète Mouhammad (PSL) et de son fidèle serviteur, Serigne Touba Khadimou Rassoul.</li>
+		<li>Offrir des locaux fonctionnels aux talibés mourides et à toute la communauté musulmane pour la célébration des événements religieux.<li>
+	</ul>
+	<h2>Impact du projet :</h2>
+	<div>Le projet présente une valeur ajoutée  certaine sur les plans social, humain et religieux. 
+		Ainsi, au-delà du fait qu’il contribuera à renforcer les liens de solidarité entre frères et sœurs musulmans, 
+		il  favorisera également l’éducation des enfants et des adultes selon les recommandations de l’islam, religion de paix et de tolérance.</div>
+
+	<div class="coutProjetKeurSerigneTouba">approximatif : 600.000$</div>
+	
+	<h2>Mode de financement :</h2>
+Le financement du complexe repose essentiellement sur la participation volontaire. 
+La fondation « Nourou Darayni » de Montréal, initiatrice du projet, a commencé à réunir  la cotisation de ses membres depuis avril  2010. 
+Elle invite tout le monde, selon les modalités suivantes, à faire un don pour  accomplir  l’œuvre d’Allah :
+	<ul>
+		<li>Par dépôt bancaire : Banque Royale du Canada : 03981-1004621</li>
+		<li>Par courriel interac à nourou.darayni@gmail.com</li>
+	</ul>
+Personnes à contacter pour toute    remise d’espèce : 
+	<ul>
+		<li>Serigne Babacar Diouf : (001) 514-538 – 9355</li>
+		<li>Serigne Cheikh Dia : (001) 514 - 938-0024</li>
+		<li>Serigne Ibrahima Ndiaye : (001) 514 – 735 – 1681.</li>
+		<li>Serigne Abdoulaye Diouf : (001) 514 – 331 – 0630.</li>
+	</ul>
+
+</div>
